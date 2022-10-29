b2486bf602b633ec8bbc26afdf812bc9e2eec8e5
GustavoOM <gumartins2001@gmail.com>
2022-10-29 00:05:01 -0300

    Carrossel pronto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,30 +9,35 @@ import headerCoracao from '../assets/coracao.svg'
 
 export function Header(){
     return(
-        <header className={styles.header}>
-            <img className={styles.logo} src={headerLogo} alt="Logo" />
-            <div className={styles.funcoes}>
-                <div className={styles.pesquisa}>
-                    <input type="text" placeholder="Pesquisar"/>
-                    <a href="">
-                        <img src={headerLupa} alt="Buscar item" />
-                    </a>
+        <>
+            <header className={styles.header}>
+                <img className={styles.logo} src={headerLogo} alt="Logo" />
+                <div className={styles.funcoes}>
+                    <div className={styles.pesquisa}>
+                        <input type="text" placeholder="Pesquisar"/>
+                        <a href="">
+                            <img src={headerLupa} alt="Buscar item" />
+                        </a>
+                    </div>
+                    <div className={styles.icones}>
+                        <a href="">
+                            <div className={styles.carrinho}>
+                                <img className={styles.carrinhoImg} src={headerCarrinho} alt="Ir para o carrinho" />
+                                <div></div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <img className={styles.perfilImg} src={headerPerfil} alt="Entrar no Perfil" />
+                        </a>
+                        <a href="">
+                            <img className={styles.coracaoImg} src={headerCoracao} alt="Ir para os favoritos" />
+                        </a>
+                    </div>
                 </div>
-                <div className={styles.icones}>
-                    <a href="">
-                        <div className={styles.carrinho}>
-                            <img className={styles.carrinhoImg} src={headerCarrinho} alt="Ir para o carrinho" />
-                            <div></div>
-                        </div>
-                    </a>
-                    <a href="">
-                        <img className={styles.perfilImg} src={headerPerfil} alt="Entrar no Perfil" />
-                    </a>
-                    <a href="">
-                        <img className={styles.coracaoImg} src={headerCoracao} alt="Ir para os favoritos" />
-                    </a>
-                </div>
+            </header>
+            <div className={styles.botoes}>
+                
             </div>
-        </header>
+        </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,31 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { Header } from "./components/Header"
 import { Content } from "./components/Content"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import Carrossel1 from './assets/Carrossel1.png'
+import Carrossel2 from './assets/Carrossel2.png'
+import Carrossel3 from './assets/Carrossel3.png'
+import Carrossel4 from './assets/Carrossel4.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
+    <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false}>
+                <div>
+                    <img src={Carrossel1} />
+                </div>
+                <div>
+                    <img src={Carrossel2} />
+                </div>
+                <div>
+                    <img src={Carrossel3} />
+                </div>
+                <div>
+                    <img src={Carrossel4} />
+                </div>
+            </Carousel>
     <Content/>
   </React.StrictMode>
 )
