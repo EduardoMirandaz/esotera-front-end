fa555affde9d7a7b06952a0921fec774a96f1bbb
Mariana Kaori <mariana.kaori@usp.br>
2022-10-30 19:32:35 -0300

    footer com imagens, falta alinhar tudo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,47 @@
 import styles from './Footer.module.css'
 
-import headerLogo from '../assets/logo.svg'
-import headerLupa from '../assets/lupa.svg'
-import headerCarrinho from '../assets/carrinho.svg'
-import headerPerfil from '../assets/perfil.svg'
-import headerCoracao from '../assets/coracao.svg'
+import footerLogo from '../assets/logoFooter.svg'
+import footerTwitter from '../assets/logoTwitter.svg'
+import footerInstagram from '../assets/logoInstagram.svg'
+import footerFacebook from '../assets/logoFacebook.svg'
 
 
 export function Footer(){
     return(
         <>
             <footer className={styles.footer}>
-                <img className={styles.logo} src={headerLogo} alt="Logo" />
+                <div className={styles.container}>
+                    <section className={styles.marca}>
+                        <img className={styles.logo} src={footerLogo} alt="Logo" />
+                        <p>A Esotera é um site de venda de produtos esotéricos desenvolvido em 2022 por um grupo de  alunos do ICMC.</p>
+                    </section>
+                    <section className={styles.informacoes}>
+                        <h1>INFORMAÇÕES</h1>
+                        <ul>
+                            <li>
+                                <a href="">Dúvidas Frequentes</a>
+                            </li>
+                            <li>
+                                <a href="">Política de Privacidade</a>
+                            </li>
+                            <li>
+                                <a href="">Fale Conosco</a>
+                            </li>
+                        </ul>    
+                    </section>
+                </div>
+                <div className={styles.redes}>
+                    <a href="">
+                        <img className={styles.twitterImg} src={footerTwitter} alt="Ir para o Twiiter de Esotera" />
+                    </a>
+                    <a href="">
+                        <img className={styles.instagramImg} src={footerInstagram} alt="Ir para o Instagram de Esotera" />
+                    </a>
+                    <a href="">
+                        <img className={styles.facebookImg} src={footerFacebook} alt="Ir para o Facebook de Esotera" />
+                    </a>
+                </div>
             </footer>
-        </>
+        </> 
     )
 }
