cf656cd7e1bf38116ea9539259829359f7d11859
Mariana Kaori <mariana.kaori@usp.br>
2022-11-01 12:43:49 -0300

    ajustes pro mobile

--- src/components/Accessibility.tsx
+++ src/components/Accessibility.tsx
@@ -6,9 +6,9 @@ import accessContraste from '../assets/iconeContraste.svg'
 export function Accessibility(){
     return(
         <div className={styles.accessibility}>
-            <img className={styles.tamFonte} src={accessFonte} alt="Ir para o Twiiter de Esotera" />
+            <img className={styles.tamFonte} src={accessFonte} alt="Aumentar a fonte" />
             <hr className={styles.linha}/>
-            <img className={styles.contraste} src={accessContraste} alt="Ir para o Instagram de Esotera" />
+            <img className={styles.contraste} src={accessContraste} alt="Aumentar o contraste" />
         </div>
     )
 }
