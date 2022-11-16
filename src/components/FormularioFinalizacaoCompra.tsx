41e85084718a32cbe30c1cdb4d1ab6c76dd22263
Mariana Kaori <mariana.kaori@usp.br>
2022-11-16 16:07:20 -0300

    formulario de compra iniciado, falta o css

---
+++ src/components/FormularioFinalizacaoCompra.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './FormularioFinalizacaoCompra.module.css'
+
+
+export function FormularioFinalizacaoCompra(){
+    return(
+        <div className={styles.containerFormulario}>
+            <form className={styles.formulario}>
+
+                <fieldset className={styles.dadosPessoais}>
+                    <legend>Dados Pessoais</legend>
+                    <input type="text" id="nomeCompleto" name="nomeCompleto" placeholder="Nome Completo" required/>
+                    <input type="text" id="cpf" name="cpf" placeholder="CPF" required/>
+                </fieldset>
+
+                <fieldset className={styles.enderecoEntrega}>
+                    <legend>Endereço de Entrega</legend>
+                    <input type="text" id="cidade" name="cidade" placeholder="Cidade" required/>
+                    <input type="text" id="estado" name="estado" placeholder="Estado" required/>
+                    <input type="text" id="logradouro" name="logradouro" placeholder="Logradouro" required/>
+                    <input type="text" id="numero" name="numero" placeholder="Número" required/>
+                    <input type="text" id="cep" name="cep" placeholder="CEP" required/>
+                    <input type="text" id="complemento" name="complemento" placeholder="Complemento" required/>
+                </fieldset> 
+
+            </form>
+        </div>
+    )
+}
--- src/pages/Carrinho.tsx
+++ src/pages/Carrinho.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Accessibility } from "../components/Accessibility";
 import { Footer } from "../components/Footer";
+import { FormularioFinalizacaoCompra } from "../components/FormularioFinalizacaoCompra";
 import { Header } from "../components/Header";
 import { Pagamento } from "../components/Pagamento";
 
@@ -9,6 +10,7 @@ export function Carrinho(){
   return(
     <>
         <Header/>
+        <FormularioFinalizacaoCompra/>
         <Pagamento/>
         <Accessibility/>
         <Footer/>
