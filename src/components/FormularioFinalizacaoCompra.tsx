2f8b3bc5fd2997963018f44d7c20b44863569488
Mariana Kaori <mariana.kaori@usp.br>
2022-11-22 16:51:27 -0300

    componente formulario de compra finalizado

--- src/components/FormularioFinalizacaoCompra.tsx
+++ src/components/FormularioFinalizacaoCompra.tsx
@@ -16,11 +16,11 @@ export function FormularioFinalizacaoCompra(){
                 <fieldset className={styles.enderecoEntrega}>
                     <legend>Endereço de Entrega</legend>
                     <input type="text" id={styles.cidade} name="cidade" placeholder="Cidade" required/>
-                    <input type="text" id="estado" name="estado" placeholder="Estado" required/>
-                    <input type="text" id="logradouro" name="logradouro" placeholder="Logradouro" required/>
-                    <input type="text" id="numero" name="numero" placeholder="Número" required/>
-                    <input type="text" id="cep" name="cep" placeholder="CEP" required/>
-                    <input type="text" id="complemento" name="complemento" placeholder="Complemento" required/>
+                    <input type="text" id={styles.estado} name="estado" placeholder="Estado" required/>
+                    <input type="text" id={styles.logradouro} name="logradouro" placeholder="Logradouro" required/>
+                    <input type="text" id={styles.numero} name="numero" placeholder="Número" required/>
+                    <input type="text" id={styles.cep} name="cep" placeholder="CEP" required/>
+                    <input type="text" id={styles.complemento} name="complemento" placeholder="Complemento" required/>
                 </fieldset> 
 
             </form>
