f75e30e4ed14fef9204659f2936b00006ffb87ef
Mariana Kaori <mariana.kaori@usp.br>
2022-11-27 01:19:50 -0300

    botão de diminuir a fonte adicionado

--- src/components/Accessibility.tsx
+++ src/components/Accessibility.tsx
@@ -1,13 +1,16 @@
 import styles from './Accessibility.module.css'
 
-import accessFonte from '../assets/iconeFonte.svg'
+import accessFonteAumentar from '../assets/iconeFonte.svg'
 import accessContraste from '../assets/iconeContraste.svg'
+import accessFonteDiminuir from '../assets/iconeFonteDiminuir.svg'
 
 export function Accessibility(){
     return(
         <div className={styles.containerAccessibility}>
             <div className={styles.accessibility}>
-                <img className={styles.tamFonte} src={accessFonte} alt="Aumentar a fonte" />
+                <img className={styles.tamFonte} src={accessFonteAumentar} alt="Aumentar a fonte" />
+                <hr className={styles.linha}/>
+                <img className={styles.tamFonte} src={accessFonteDiminuir} alt="Diminuir a fonte" />
                 <hr className={styles.linha}/>
                 <img className={styles.contraste} src={accessContraste} alt="Aumentar o contraste" />
             </div>
