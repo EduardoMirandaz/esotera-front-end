8aea92b43a034431328acfd9bc0fd5eb99f9e133
Mariana Kaori <mariana.kaori@usp.br>
2022-11-27 02:37:53 -0300

    pagina de carrinho alinhada (até agora)

--- src/pages/Carrinho.tsx
+++ src/pages/Carrinho.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Carrinho.module.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Accessibility } from "../components/Accessibility";
 import { Footer } from "../components/Footer";
@@ -11,21 +12,31 @@ export function Carrinho(props) {
   return (
     <>
       <Header />
-      <FormularioFinalizacaoCompra />
-      <Pagamento />
-      {props.produto.map((card) => {
-        if (card.idProduto == 0) {
-          return (
-            <CardCompra
-              descricao={card.descricao}
-              imagemPrincipal={card.imagemPrincipal}
-              valor={card.valor}
-              idProduto={card.idProduto}
-            />
-          )
-        }
-      })
-      }
+      <div className={styles.container}>
+        <div className={styles.right}>
+          <h1 className={styles.titulo}>Finalizar compra</h1>
+          <FormularioFinalizacaoCompra />
+          <Pagamento />
+        </div>
+        <div className={styles.left}>
+          <h2 className={styles.titulo}>Itens do Carrinho</h2>
+          <div className={styles.boxCardsCompras}>
+            {props.produto.map((card) => {
+              if (card.idProduto == 0) {
+                return (
+                  <CardCompra
+                    descricao={card.descricao}
+                    imagemPrincipal={card.imagemPrincipal}
+                    valor={card.valor}
+                    idProduto={card.idProduto}
+                  />
+                )
+              }
+            })
+            }
+          </div>
+        </div>
+      </div>
       <Accessibility />
       <Footer />
     </>
