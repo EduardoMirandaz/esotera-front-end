e0f0c0a6ebd40d506d4f623c575808cd8560977a
Hugo Ferreira <huugo.vieira49@gmail.com>
2022-11-29 18:23:55 -0300

    Fazendo o cadastro

--- src/components/Cadastro.tsx
+++ src/components/Cadastro.tsx
@@ -1,24 +1,28 @@
-import styles from './Login.module.css';
-import React, { useState } from 'react';
+import styles from './Cadastro.module.css';
+import React from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
-import Cadastro from './Cadastro';
 
-export function Login({showModal, setShowModal}){
-    const [showModalCad, setShowModalCad] = useState(false)
-    const openModalCad = () => {
-        setShowModalCad(prev => !prev)
-        setShowModal(prev => !prev)
-    }
+
+export function Cadastro({showModalCad, setShowModalCad}){
+   
     return(
         <>
-        {showModal ? (
-            <Modal open={showModal} onClose={() => {setShowModal(prev => !prev)}} showModal={showModal} classNames={{ modal: styles.customModal }}  center>
+        {showModalCad ? (
+            <Modal open={showModalCad} onClose={() => {setShowModalCad(prev => !prev)}} showModalCad={showModalCad} classNames={{ modal: styles.customModal }}  center>
                 <div className={styles.modalBody}>
                     <div className={styles.modalHeader}>
-                        <h2>Entrar</h2>
+                        <h2>Cadastro</h2>
                     </div>
                     <form>
+                        <div className={styles.inputWrapper}>
+                            <label htmlFor="nome">Nome</label>
+                            <input type="text" name="nome" placeholder="" aria-required="true" required/>
+                        </div>
+                        <div className={styles.inputWrapper}>
+                            <label htmlFor="celular">Celular</label>
+                            <input type="tel" name="celular" placeholder="" aria-required="true" required/>
+                        </div>
                         <div className={styles.inputWrapper}>
                             <label htmlFor="email">E-mail</label>
                             <input type="email" name="email" placeholder="" aria-required="true" required/>
@@ -26,15 +30,14 @@ export function Login({showModal, setShowModal}){
                         <div className={styles.inputWrapper}>
                             <label htmlFor="senha">Senha</label>
                             <input type="password" name="senha" placeholder="" aria-required="true" required/>
-                            <a className={styles.esqueceuSenha} href="">Esqueceu sua senha?</a>
                         </div>
-                        <button type="submit" className={styles.button}>ENTRAR</button>
-                        <div onClick={openModalCad} className={styles.cadastrar}>
-                            <span>Não possui conta?&nbsp;&nbsp;</span>
-                            <span className={styles.cadastre}>CADASTRE-SE</span>
+                        <div className={styles.inputWrapper}>
+                            <label htmlFor="senha">Redigite sua senha</label>
+                            <input type="password" name="redigite" placeholder="" aria-required="true" required/>
                         </div>
+                        <button type="submit" className={styles.button}>CADASTRAR</button>
                         <div className={styles.divider}>
-                            <span role="text" aria-label="Ou entre com uma dessas opções">ou entre com</span>
+                            <span role="text" aria-label="Ou cadastre-se com uma dessas opções">ou cadastre-se com</span>
                             <hr/>
                         </div>
                         <div className={styles.socialNetworkWrapper}>
@@ -49,11 +52,9 @@ export function Login({showModal, setShowModal}){
                 </div>
                 
             </Modal>
-            
         ) : null}
-        <Cadastro showModalCad={showModalCad} setShowModalCad={setShowModalCad} />
         </>
     )
 }
 
-export default Login
+export default Cadastro
