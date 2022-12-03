3fe7e9586bc9cd7be46409f983d54fe7e0dfe612
Mariana Kaori <mariana.kaori@usp.br>
2022-12-03 01:22:15 -0300

    imagens de produtos na pagina de produto

--- src/pages/Produto.tsx
+++ src/pages/Produto.tsx
@@ -6,14 +6,25 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Vitrine } from "../components/Vitrine";
 import { SumarioProduto } from "../components/SumarioProduto";
+import ImagensProdutos from "../components/ImagensProdutos"
+import fotoProduto from "../assets/fotoProduto.png"
+import styles from './Produto.module.css'
+
 
 export function Produto(){
   return(
     <>
-        <Header/>
-        <SumarioProduto/>
-        <Accessibility/>
-        <Footer/>
+      <Header/>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <ImagensProdutos idProduto={0}/>
+        </div>
+        <div className={styles.right}>
+          <SumarioProduto/>
+        </div>
+      </div>
+      <Accessibility/>
+      <Footer/>
     </>
   );
 }
