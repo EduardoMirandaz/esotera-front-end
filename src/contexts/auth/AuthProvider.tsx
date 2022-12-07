4b082887a4a9b18ebb2846e2f82d3dd469382f51
GustavoOM <gumartins2001@gmail.com>
2022-12-07 15:42:01 -0300

    Context de contraste

--- src/contexts/auth/AuthProvider.tsx
+++ src/contexts/auth/AuthProvider.tsx
@@ -10,9 +10,10 @@ export default function AuthProvider(props: AuthProviderProps) {
     const [usuario, setUsuario] = useState("");
     const [carrinho, setCarrinho] = useState("");
     const [filtro, setFiltro] = useState("");
+    const [contraste, setContraste] = useState(false);
 
     return (
-        <AuthContext.Provider value={{usuario, setUsuario, carrinho, setCarrinho, filtro, setFiltro}}>
+        <AuthContext.Provider value={{usuario, setUsuario, carrinho, setCarrinho, filtro, setFiltro, contraste, setContraste}}>
             {props.children}
         </AuthContext.Provider>
     )
