43e63855de614738b89f0fc2a1acdd2754dfab84
GustavoOM <gumartins2001@gmail.com>
2022-12-07 22:19:24 -0300

    Contraste Produto Finalizado

--- src/components/SumarioProduto.tsx
+++ src/components/SumarioProduto.tsx
@@ -3,21 +3,28 @@ import styles from './SumarioProduto.module.css'
 import estrela from '../assets/star.svg'
 import meiaEstrela from '../assets/star-half.svg'
 import carrinho from '../assets/carrinho.svg'
-import mapPin from '../assets/map-pin.svg'
-import coracaoVazio from '../assets/coracaoVazioPurple500.svg'
+import map from '../assets/mapa.svg'
+import mapContraste from '../assets/mapaContraste.svg'
 import { produto } from '../data.json'
-
+import { useAuthContext } from '../contexts/auth/AuthContext'
+import coracaoCheio from '../assets/coracaoCheio.svg'
+import coracaoVazio from '../assets/coracaoVazio.svg'
+import coracaoCheioContraste from '../assets/coracaoCheioContraste.svg'
+import coracaoVazioContraste from '../assets/coracaoVazioContraste.svg'
 
 export function SumarioProduto(props){
     const [contador, setCount] = useState(1); 
     // useState returns a pair. 'count' is the current state. 'setCount' is a function we can use to update the state.
-    
+    const { contraste } = useAuthContext();
+  
     function incrementar() {
       //setCount(prevCount => prevCount+=s1);
       setCount(function (prevCount) {
         return (prevCount += 1);
       });
     }
+
+    const [isClick, setClick] = useState(false);
     
     function decrementar() {
       setCount(function (prevCount) {
@@ -29,10 +36,10 @@ export function SumarioProduto(props){
       });
     }
     return(
-        <div className={styles.containerSumario}>
+        <div className={styles.containerSumario} id={contraste && styles.contraste}>
             <h1 className={styles.titulo}>{produto[props.idProduto].titulo}</h1>
             <div className={styles.subCategorias}>
-                <p className={styles.estoque}>Em estoque</p>
+                <p className={styles.estoque} id={contraste && styles.contraste}>Em estoque</p>
                 <p className={styles.comprados}>{produto[props.idProduto].qtdComprados} Comprados</p>
                 <div className={styles.estrelas}>
                     <img src={estrela} alt="Avaliação"/>
@@ -47,13 +54,35 @@ export function SumarioProduto(props){
             <p className={styles.texto}>{produto[props.idProduto].descricaoDetalhadaMenor}</p>
             <div className={styles.botoesProduto}>
                 <div className={styles.contador}>
-                    <button className={styles.decrementar} onClick={decrementar}>−</button>
-                    <h1>{contador}</h1>
-                    <button className={styles.incrementar} onClick={incrementar}>+</button>
+                    <button className={styles.decrementar} onClick={decrementar} id={contraste && styles.contraste}>−</button>
+                    <h1 className={styles.quantidade} id={contraste && styles.contraste}>{contador}</h1>
+                    <button className={styles.incrementar} onClick={incrementar} id={contraste && styles.contraste}>+</button>
                 </div> 
-                <button className={styles.botaoCarrinho}>Adicionar ao carrinho<img src={carrinho} alt="Adicionar ao carrinho"/></button>
-                <button className={styles.botaoFrete}>Calcular o frete<img src={mapPin} alt="Calcular o frete"/></button>
-                <button className={styles.botaoFavorito}><img src={coracaoVazio} alt="Adicionar aos favoritos"/></button>
+                <button className={styles.botaoCarrinho} id={contraste && styles.contraste}>Adicionar ao carrinho<img src={carrinho} alt="Adicionar ao carrinho"/></button>
+                <button className={styles.botaoFrete} id={contraste && styles.contraste}>Calcular o frete
+                  {
+                    contraste &&
+                    <img src={mapContraste} alt="Calcular o frete"/>
+                  }
+                  {
+                    !contraste &&
+                    <img src={map} alt="Calcular o frete"/>
+                  }
+                </button>
+                <button className={styles.botaoFavorito} id={contraste && styles.contraste} onClick={() => setClick(!isClick)}>
+                  {isClick && contraste &&
+                    <img src={coracaoCheioContraste}/>
+                  }
+                  {!isClick && contraste &&
+                    <img src={coracaoVazioContraste}/>
+                  }
+                  {isClick && !contraste &&
+                    <img src={coracaoCheio}/>
+                  }
+                  {!isClick && !contraste &&
+                    <img src={coracaoVazio}/>
+                  }
+                </button>
             </div>
         </div>
     )
