a77179ee8caf093ca8a36bff83bcea88932c245d
Eduardo Costa Miranda Azevedo <azevedo_eduardo@sicredi.com.br>
2022-12-09 21:00:33 -0300

    Ultimo ajuste no logout.

--- src/components/MenuLogout.tsx
+++ src/components/MenuLogout.tsx
@@ -1,7 +1,7 @@
 import styles from './MenuLogout.module.css'; 
 import React, { useState } from 'react';
 import { useAuthContext } from '../contexts/auth/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export function MenuLogout() {
 
@@ -17,10 +17,10 @@ export function MenuLogout() {
     const [ setCarrinhoList] = useState(getCarrinhoList());
     
     const deslogar = () => {
+        window.location.reload();
         setModalLogout(prev => !prev);
         localStorage.removeItem("username");
         localStorage.setItem("carrinho", "[]"); setCarrinhoList([]);
-        navigateToPrincipal();
     }
 
 
@@ -29,7 +29,7 @@ export function MenuLogout() {
         <div className={styles.wrapper}>
             <div className={styles.caixaInternaDoLogout} id={contraste && styles.contraste}>
                 <p className={styles.desejaSair}>Deseja sair da conta?</p>
-                <button type="button" onClick={deslogar} className={styles.botaoSair} aria-label="Sair da conta">SAIR</button>
+                <Link to="/" type="button" onClick={deslogar} className={styles.botaoSair} aria-label="Sair da conta">SAIR</Link>
             </div>
         </div>
             <div className={styles.setaDaCaixaInternaDoLogout}>
